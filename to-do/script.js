541e6b5ee69a0cb8f9e7fb89ae624eb2cc03f9f2
Arkit Karmokar <arkitkarmokar007@gmail.com>
2024-09-16 22:40:49 +0530

    Add files via upload

---
+++ to-do/script.js
@@ -0,0 +1,34 @@
+document.querySelector('#push').onclick = function() {
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("Please enter a task");
+    }
+    else {
+        document.querySelector('#tasks').innerHTML
+        += `
+        <div class="task">
+            <span id = "taskname">
+                ${document.querySelector
+                ('#newtask input').value}
+            </span>
+            <button class="delete">
+            <i class="fa-solid fa-trash"></i>
+            </button>
+        </div>
+        `;
+
+        var current_task = document.querySelectorAll('.delete');
+        for(var i=0; i<current_task.length; i++) {  
+            current_task[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+        var tasks = document.querySelectorAll(".task");
+            for(var i=0; i<tasks.length; i++) {
+                tasks[i].onclick = function(){
+                    this.classList.toggle('completed');
+            }
+        }
+        document.querySelector("#newtask input").
+        value = " ";
+    }
+}
